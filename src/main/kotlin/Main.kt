d9bbb15f55aa366c2cc9534c281f6f8f8fdf7b6a
Aznos <91508926+Aznos@users.noreply.github.com>
2025-08-27 13:32:28 -0400

    reading files

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
-fun main() {
-    println("Hello world")
+import io.Source
+import io.SourceLoader
+
+/**
+ * Main entry point
+ */
+fun main(args: Array<String>) {
+    val source: Source = if(args.isNotEmpty()) {
+        SourceLoader.fromFile(args[0])
+    } else {
+        println("Reading program from stdin. Press Ctrl+D (Unix) or Ctrl+Z (Windows) to end input.")
+        SourceLoader.fromStdin()
+    }
+
+    println("Loaded: ${source.origin}")
+    println("Line count: ${source.lineCount}")
+    println("---------- Program Start ----------")
+
+    source.lines().forEachIndexed { idx, line ->
+        val n = idx + 1 // Line numbers start at 1 for user display
+        println(String.format("%4d | %s", n, line))
+    }
 }
