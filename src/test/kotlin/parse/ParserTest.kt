8650dd2dd3d357a84bbfa929809860c83014e268
Aznos <91508926+Aznos@users.noreply.github.com>
2025-08-28 13:01:01 -0400

    Parser tests

---
+++ src/test/kotlin/parse/ParserTest.kt
@@ -0,0 +1,24 @@
+package parse
+
+import io.Source
+import lexer.Lexer
+import kotlin.test.Test
+import kotlin.test.assertFailsWith
+
+class ParserTest {
+    private fun parseOk(src: String) = Parser(Lexer(Source.fromRaw("t", src)).lexAll()).parseProgram()
+
+    @Test fun parsePlaceSack() {
+        parseOk("place sack in slot 1 contains [pumpkin, melon_block]")
+    }
+
+    @Test fun parseSayHarvest() {
+        parseOk("say harvest  slot 1 at 2 to string")
+    }
+
+    @Test fun redstoneRequiresThen() {
+        assertFailsWith<ParseException> {
+            parseOk("redstone slot 1 bedrock slot 2\n   say cobblestone\nend")
+        }
+    }
+}
