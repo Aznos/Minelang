0a9d537c81b2be15794703a51691c81b33f04e53
Aznos <91508926+Aznos@users.noreply.github.com>
2025-08-28 13:07:12 -0400

    Control flow test

---
+++ src/test/kotlin/runtime/ControlFlowTest.kt
@@ -0,0 +1,20 @@
+package runtime
+
+import support.runAndAssert
+import kotlin.test.Test
+
+class ControlFlowTest {
+    @Test fun redstoneElseMineTravelLengthHarvest_printsHello() {
+        runAndAssert(
+            """
+                place sack in slot 1 contains [pumpkin_stem, iron_bars, brick_stairs, brick_stairs, waterlily]
+                length slot 1 into slot 2
+                place stone in slot 4
+                travel slot 3 from slot 4 to slot 2 do
+                    say harvest slot 1 at slot 3 to string
+                end
+            """.trimIndent(),
+            "hello"
+        )
+    }
+}
